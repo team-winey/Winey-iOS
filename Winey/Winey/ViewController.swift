b508af7779bed9ec8296ef00cca577e40f8d1c0e
6uohul <kiy010604@naver.com>
2023-07-01 21:17:41 +0900

    [Add] #1 - 프로젝트 초기 생성

---
+++ Winey/Winey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Winey
+//
+//  Created by 김인영 on 2023/07/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
