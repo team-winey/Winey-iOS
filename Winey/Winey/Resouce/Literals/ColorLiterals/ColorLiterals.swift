e22636c3921f764ba2ed706546de3a6e0652bc21
6uohul <kiy010604@naver.com>
2023-07-05 18:30:46 +0900

    [Feat] #1- ColorLiterals 추가

---
+++ Winey/Winey/Resouce/Literals/ColorLiterals/ColorLiterals.swift
@@ -0,0 +1,92 @@
+//
+//  ColorLiterals.swift
+//  Winey
+//
+//  Created by 김인영 on 2023/07/05.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    @nonobjc class var winey_yellow: UIColor {
+        return UIColor(red: 0.8, green: 0.988, blue: 0.337, alpha: 1)
+    }
+
+    @nonobjc class var winey_purple100: UIColor {
+        return UIColor(red: 0.949, green: 0.914, blue: 1, alpha: 1)
+    }
+
+    @nonobjc class var winey_purple200: UIColor {
+        return UIColor(red: 0.788, green: 0.651, blue: 0.992, alpha: 1)
+    }
+
+    @nonobjc class var winey_purple300: UIColor {
+        return UIColor(red: 0.635, green: 0.4, blue: 0.988, alpha: 1)
+    }
+
+    @nonobjc class var winey_purple400: UIColor {
+        return UIColor(red: 0.486, green: 0.149, blue: 0.98, alpha: 1)
+    }
+
+    @nonobjc class var winey_purple500: UIColor {
+        return UIColor(red: 0.298, green: 0.09, blue: 0.6, alpha: 1)
+    }
+
+    @nonobjc class var winey_gray0: UIColor {
+        return UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+    }
+
+    @nonobjc class var winey_gray50: UIColor {
+        return UIColor(red: 0.969, green: 0.973, blue: 0.98, alpha: 1)
+    }
+
+    @nonobjc class var winey_gray100: UIColor {
+        return UIColor(red: 0.941, green: 0.949, blue: 0.961, alpha: 1)
+    }
+
+    @nonobjc class var winey_gray200: UIColor {
+        return UIColor(red: 0.878, green: 0.898, blue: 0.922, alpha: 1)
+    }
+
+    @nonobjc class var winey_gray300: UIColor {
+        return UIColor(red: 0.749, green: 0.78, blue: 0.808, alpha: 1)
+    }
+
+    @nonobjc class var winey_gray400: UIColor {
+        return UIColor(red: 0.592, green: 0.635, blue: 0.686, alpha: 1)
+    }
+
+    @nonobjc class var winey_gray500: UIColor {
+        return UIColor(red: 0.459, green: 0.522, blue: 0.584, alpha: 1)
+    }
+
+    @nonobjc class var winey_gray600: UIColor {
+        return UIColor(red: 0.322, green: 0.4, blue: 0.478, alpha: 1)
+    }
+
+    @nonobjc class var winey_gray700: UIColor {
+        return UIColor(red: 0.227, green: 0.282, blue: 0.263, alpha: 1)
+    }
+
+    @nonobjc class var winey_gray800: UIColor {
+        return UIColor(red: 0.078, green: 0.098, blue: 0.122, alpha: 1)
+    }
+
+    @nonobjc class var winey_gray900: UIColor {
+        return UIColor(red: 0.078, green: 0.098, blue: 0.122, alpha: 1)
+    }
+
+    @nonobjc class var winey_red: UIColor {
+        return UIColor(red: 1, green: 0.318, blue: 0.314, alpha: 1)
+    }
+
+    @nonobjc class var winey_blue: UIColor {
+        return UIColor(red: 0.153, green: 0.431, blue: 0.949, alpha: 1)
+    }
+
+    @nonobjc class var winey_green: UIColor {
+        return UIColor(red: 0, green: 0.796, blue: 0.506, alpha: 1)
+    }
+}
+
