ab96ecb24150d0acaedd6050dd2e070ad11da1e9
6uohul <kiy010604@naver.com>
2023-07-05 11:18:48 +0900

    [Feat] #1- Date+ Extension 추가

---
+++ Winey/Winey/Source/Extension/Date+.swift
@@ -0,0 +1,18 @@
+//
+//  Date+.swift
+//  Winey
+//
+//  Created by 김인영 on 2023/07/05.
+//
+
+import UIKit
+import Foundation
+
+extension Date {
+    func getTodayDateToString(_ format: String) -> String {
+        var dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = format
+        let today = dateFormatter.string(from: Date())
+        return today
+    }
+}
