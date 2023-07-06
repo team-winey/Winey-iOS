4ac2e04b49f49a2e29dbc9e5502e66e15e3e9008
6uohul <kiy010604@naver.com>
2023-07-06 16:34:28 +0900

    [Feat] #1- ImageLiterals 추가

---
+++ Winey/Winey/Resource/Literals/ImageLiterals/ImageLiterals.swift
@@ -0,0 +1,20 @@
+//
+//  ImageLiterals.swift
+//  Winey
+//
+//  Created by 김인영 on 2023/07/06.
+//
+
+import UIKit
+
+struct ImageLiterals {
+    
+    struct TabBar {
+        
+        static let feed = UIImage(named: "icn_feed")
+        
+        static let recommend = UIImage(named: "icn_recommend")
+        
+        static let myPage = UIImage(named: "icn_myPage")
+    }
+}
