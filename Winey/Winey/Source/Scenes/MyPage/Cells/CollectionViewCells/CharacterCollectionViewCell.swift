ed3914c8c9e0d9f224c318eb1c6d9a18b09f636b
GodSamble <osgneb8359@naver.com>
2023-07-10 18:53:45 +0900

    [Add] #4 - 마이페이지 청사진에 기반한 세분화한 파일들 추가

---
+++ Winey/Winey/Source/Scenes/MyPage/Cells/CollectionViewCells/CharacterCollectionViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  CharacterCollectionViewCell.swift
+//  Winey
+//
+//  Created by 고영민 on 2023/07/10.
+//
+
+import Foundation
---
+++ Winey/Winey/Source/Scenes/MyPage/Cells/CollectionViewCells/SetupCollectionViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  SetupCollectionViewCell.swift
+//  Winey
+//
+//  Created by 고영민 on 2023/07/10.
+//
+
+import Foundation
