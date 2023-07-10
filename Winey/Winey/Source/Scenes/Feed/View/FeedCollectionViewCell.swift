270e895a1573abae8c4db8afb63a18fb7daf859e
6uohul <kiy010604@naver.com>
2023-07-11 02:26:20 +0900

    [Add] #3- Diffable Datasource로 CollectionView 생성

---
+++ Winey/Winey/Source/Scenes/Feed/View/FeedCollectionViewCell.swift
@@ -0,0 +1,58 @@
+//
+//  FeedCollectionViewCell.swift
+//  Winey
+//
+//  Created by 김인영 on 2023/07/10.
+//
+
+import UIKit
+
+import SnapKit
+
+struct FeedItem: Hashable {
+    let nickname: String
+    let feedTitle: String
+    let feedImage: UIImage
+    let feedMoney: Int
+    let likes: Int
+    var isLiked: Bool
+    let createdAt: Date
+    let writerLevel: Int
+}
+
+class FeedCollectionViewCell: UICollectionViewCell {
+    
+    private var nicknameLabel = UILabel()
+    private var feedTitleLabel = UILabel()
+    private var feedImage = UIImage()
+    private var feedMoneyLabel = UILabel()
+    private var likesLabel = UILabel()
+    private var likeButton = UIButton()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func dataBind(model: FeedItem) {
+        feedTitleLabel.text = model.feedTitle
+    }
+}
+
+extension FeedCollectionViewCell {
+    private func setLayout() {
+        addSubviews(feedTitleLabel)
+        
+        backgroundColor = .green
+        feedTitleLabel.textColor = .white
+        
+        feedTitleLabel.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+}
+
