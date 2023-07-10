d529f8ee49eb44ae86c24ddc0ff6cdda10fc5ba1
김응관 <kwhn9486@naver.com>
2023-07-11 02:46:33 +0900

    [Add] #8 - 너비를 기준으로 이미지 크기를 훼손없이 줄어져는 UIImage 관련 Extension 함수추가

--- Winey/Winey/Source/Global/Extension/UIKit/UIImage+.swift
+++ Winey/Winey/Source/Global/Extension/UIKit/UIImage+.swift
@@ -28,4 +28,12 @@ extension UIImage {
         draw(in: CGRect(origin: .zero, size: canvasSize))
         return UIGraphicsGetImageFromCurrentImageContext()
     }
+    
+    func resized(toWidth width: CGFloat) -> UIImage? {
+        let canvasSize = CGSize(width: width, height: CGFloat(ceil(width/size.width * size.height)))
+        UIGraphicsBeginImageContextWithOptions(canvasSize, false, scale)
+        defer { UIGraphicsEndImageContext() }
+        draw(in: CGRect(origin: .zero, size: canvasSize))
+        return UIGraphicsGetImageFromCurrentImageContext()
+    }
 }
