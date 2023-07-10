4a14ae8f34f6bf58188ea2bca17686fdf9e103af
6uohul <kiy010604@naver.com>
2023-07-10 18:31:54 +0900

    [Add] #3- 피드 CollectionViewCell 추가

--- Winey/Winey/Source/Scenes/Feed/Views/FeedCollectionViewCell.swift
+++ Winey/Winey/Source/Scenes/Feed/Views/FeedCollectionViewCell.swift
@@ -9,4 +9,11 @@ import UIKit
 
 class FeedCollectionViewCell: UICollectionViewCell {
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
