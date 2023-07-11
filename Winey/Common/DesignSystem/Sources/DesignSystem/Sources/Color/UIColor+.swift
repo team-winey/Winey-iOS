ceb0c6be81d7d06d511e99837ce705343fbac08b
samsung-ga <maxwodyd98@gmail.com>
2023-07-11 22:21:07 +0900

    [FEAT] #9 - 디자인시스템 모듈에 Color 정의 방식 변경

---
+++ Winey/Common/DesignSystem/Sources/DesignSystem/Sources/Color/UIColor+.swift
@@ -0,0 +1,34 @@
+//
+//  ColorLiterals.swift
+//  Winey
+//
+//  Created by 김인영 on 2023/07/05.
+//
+
+import UIKit
+
+public extension UIColor {
+    @nonobjc static var mainYellow = UIColor(pallete: .mainYellow)
+    
+    @nonobjc static var blue500    = UIColor(pallete: .blue500)
+    @nonobjc static var green500   = UIColor(pallete: .green500)
+    @nonobjc static var red500     = UIColor(pallete: .red500)
+    
+    @nonobjc static var purple100  = UIColor(pallete: .purple100)
+    @nonobjc static var purple200  = UIColor(pallete: .purple200)
+    @nonobjc static var purple300  = UIColor(pallete: .purple300)
+    @nonobjc static var purple400  = UIColor(pallete: .purple400)
+    @nonobjc static var purple500  = UIColor(pallete: .purple500)
+    
+    @nonobjc static var gray0      = UIColor(pallete: .gray0)
+    @nonobjc static var gray50     = UIColor(pallete: .gray50)
+    @nonobjc static var gray100    = UIColor(pallete: .gray100)
+    @nonobjc static var gray200    = UIColor(pallete: .gray200)
+    @nonobjc static var gray300    = UIColor(pallete: .gray300)
+    @nonobjc static var gray400    = UIColor(pallete: .gray400)
+    @nonobjc static var gray500    = UIColor(pallete: .gray500)
+    @nonobjc static var gray600    = UIColor(pallete: .gray600)
+    @nonobjc static var gray700    = UIColor(pallete: .gray700)
+    @nonobjc static var gray800    = UIColor(pallete: .gray800)
+    @nonobjc static var gray900    = UIColor(pallete: .gray900)
+}
