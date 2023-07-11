7ec7a49600bdb8760466830a2a4b7a0a0c80e9c2
6uohul <kiy010604@naver.com>
2023-07-12 03:33:31 +0900

    [Feat] #3- 피드 헤더뷰 구현

--- Winey/Winey/Source/Scenes/Feed/View/FeedCollectionReusableHeaderView.swift
+++ Winey/Winey/Source/Scenes/Feed/View/FeedCollectionReusableHeaderView.swift
@@ -11,7 +11,35 @@ import SnapKit
 
 final class FeedCollectionReusableHeaderView: UICollectionReusableView {
     
-    private let introLabel = UILabel()
+    private let introLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.text = "이웃나라 왕족들의 소비생활을\n관찰하고 소통할 수 있어요!"
+        label.font = .head_b20
+        label.textColor = .winey_gray900
+        return label
+    }()
+    
+    private let containerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .winey_gray100
+        return view
+    }()
+    
+    private let subTitleLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.text = "아니, 어제만해도 왕족인 내가 평민이라고?\n용납할 수 없지. 얘들아 모여봐 뭔일이야"
+        label.font = .body3_m14
+        label.textColor = .winey_gray600
+        return label
+    }()
+    
+    private let characterImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = .Sample.temp
+        return imageView
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -23,13 +51,33 @@ final class FeedCollectionReusableHeaderView: UICollectionReusableView {
     }
     
     private func setLayout(){
-        introLabel.text = "랄랄라"
-        backgroundColor = .winey_gray300
         
-        addSubviews(introLabel)
+        backgroundColor = .winey_gray0
+        
+        self.addSubviews(introLabel, containerView, characterImageView)
+        containerView.addSubview(subTitleLabel)
         
         introLabel.snp.makeConstraints {
-            $0.center.equalToSuperview()
+            $0.top.equalToSuperview().inset(19)
+            $0.leading.equalToSuperview().inset(28)
+        }
+        
+        containerView.snp.makeConstraints {
+            $0.top.equalTo(introLabel.snp.bottom).offset(20)
+            $0.leading.trailing.equalToSuperview().inset(16)
+            $0.height.equalTo(77)
+        }
+        
+        subTitleLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().inset(17)
+        }
+        
+        characterImageView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(18)
+            $0.trailing.equalTo(containerView)
+            $0.width.equalTo(112)
+            $0.height.equalTo(140)
         }
     }
 }
