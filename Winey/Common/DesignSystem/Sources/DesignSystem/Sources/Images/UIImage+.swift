992f8c28985e9041a030deeaced2a0792001c670
samsung-ga <maxwodyd98@gmail.com>
2023-07-11 22:38:08 +0900

    [FEAT] #9 - UIImage+ 파일 다지인시스템으로 이동 및 정의 방식 변경

---
+++ Winey/Common/DesignSystem/Sources/DesignSystem/Sources/Images/UIImage+.swift
@@ -0,0 +1,17 @@
+//
+//  ImageLiterals.swift
+//  Winey
+//
+//  Created by 김인영 on 2023/07/06.
+//
+
+import UIKit
+
+extension UIImage {
+
+    enum Icon {
+        static let feed         = UIImage(named: "ic_feed")
+        static let recommend    = UIImage(named: "ic_recommend")
+        static let mypage       = UIImage(named: "ic_mypage")
+    }
+}
