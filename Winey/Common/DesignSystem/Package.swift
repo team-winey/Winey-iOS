f1b146096b65a1fcbe0946994e002314036e24a0
samsung-ga <maxwodyd98@gmail.com>
2023-07-11 21:09:26 +0900

    [FEAT] #9 - design system에 snapkit 디펜던시 추가

--- Winey/Common/DesignSystem/Package.swift
+++ Winey/Common/DesignSystem/Package.swift
@@ -6,21 +6,25 @@ import PackageDescription
 let package = Package(
     name: "DesignSystem",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "DesignSystem",
             targets: ["DesignSystem"]),
     ],
     dependencies: [
+        .package(
+            url: "https://github.com/SnapKit/SnapKit",
+            .upToNextMajor(from: "5.0.0")
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "DesignSystem",
-            dependencies: []),
+            dependencies: [
+                .product(name: "SnapKit", package: "SnapKit")
+            ]),
         .testTarget(
             name: "DesignSystemTests",
-            dependencies: ["DesignSystem"]),
+            dependencies: ["DesignSystem"]
+        ),
     ]
 )
