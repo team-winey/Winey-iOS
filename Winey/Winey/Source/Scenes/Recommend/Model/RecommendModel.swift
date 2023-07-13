21611f728fbe4249b440394414964c8af6ac148c
6uohul <kiy010604@naver.com>
2023-07-14 03:00:44 +0900

    [Fix] #18- 추천 모델 수정

--- Winey/Winey/Source/Scenes/Recommend/Model/RecommendModel.swift
+++ Winey/Winey/Source/Scenes/Recommend/Model/RecommendModel.swift
@@ -5,4 +5,13 @@
 //  Created by 김인영 on 2023/07/13.
 //
 
-import Foundation
+import UIKit
+
+struct RecommendModel: Hashable {
+    let id: Int
+    let link: String
+    let title: String
+    let subTitle: String
+    let discount: String
+    let image: UIImage
+}
