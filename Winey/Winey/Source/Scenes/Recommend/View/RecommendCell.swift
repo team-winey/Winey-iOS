a24a34bd9abee3e433183085e95d1f62c4e64575
6uohul <kiy010604@naver.com>
2023-07-14 06:32:06 +0900

    [Feat] #18- text 스택뷰 전체적인 틀 구현

--- Winey/Winey/Source/Scenes/Recommend/View/RecommendCell.swift
+++ Winey/Winey/Source/Scenes/Recommend/View/RecommendCell.swift
@@ -10,14 +10,29 @@ import UIKit
 import DesignSystem
 import SnapKit
 
-class RecommendCell: UICollectionViewCell {
+final class RecommendCell: UICollectionViewCell {
     
-    private let testView = UIView()
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.makeCornerRound(radius: 6)
+        return imageView
+    }()
+    
+    private let imageView1: UIImageView = {
+        let imageView = UIImageView()
+        imageView.makeCornerRound(radius: 6)
+        return imageView
+    }()
+    
+    private let discountLabel = UILabel()
+    private let titleLabel = UILabel()
+    private let separatorLineView = UIView()
+    private let linkTitleLabel = UILabel()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        contentView.backgroundColor = .winey_gray300
-        testView.backgroundColor = .winey_purple300
+        contentView.makeCornerRound(radius: 10)
+        contentView.backgroundColor = .winey_blue500
         setLayout()
     }
     
@@ -25,14 +40,109 @@ class RecommendCell: UICollectionViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
+    func configure(model: RecommendModel) {
+        imageView.image = model.image
+        discountLabel.setText(model.discount, attributes: Const.discountAttributes)
+        print(model.title)
+        titleLabel.setText(model.title, attributes: Const.titleAttributes)
+        titleLabel.numberOfLines = 2
+    }
 }
 
 extension RecommendCell {
     private func setLayout() {
-        contentView.addSubviews(testView)
+        let containerStackView = UIStackView()
+        containerStackView.axis = .vertical
+        containerStackView.spacing = 0
+        containerStackView.alignment = .fill
+        containerStackView.distribution = .fill
         
-        testView.snp.makeConstraints {
+        contentView.addSubviews(containerStackView)
+        containerStackView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
+        
+        let firstContainerView = UIView()
+        let textStackView = UIStackView()
+        textStackView.axis = .vertical
+        textStackView.spacing = 2
+        textStackView.alignment = .leading
+        textStackView.distribution = .fill
+        
+        containerStackView.addArrangedSubview(firstContainerView)
+        firstContainerView.addSubviews(imageView, textStackView)
+        textStackView.addArrangedSubviews(discountLabel, titleLabel)
+        
+        textStackView.snp.makeConstraints {
+            $0.centerY.equalTo(imageView)
+            $0.leading.equalTo(imageView.snp.trailing).offset(13)
+            $0.trailing.equalToSuperview().inset(16)
+        }
+        
+        let secondContainerView = UIView()
+        containerStackView.addArrangedSubviews(separatorLineView, secondContainerView)
+        
+        firstContainerView.backgroundColor = .winey_red500
+        secondContainerView.backgroundColor = .winey_gray700
+        separatorLineView.backgroundColor = .winey_yellow
+        imageView.backgroundColor = .winey_gray700
+        
+        firstContainerView.addSubviews(imageView)
+        secondContainerView.addSubviews(linkTitleLabel)
+        
+        separatorLineView.snp.makeConstraints {
+            $0.height.equalTo(1)
+        }
+        
+        imageView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(Const.imageViewTopSpacing)
+            $0.leading.equalToSuperview().inset(16)
+            $0.bottom.equalToSuperview().inset(Const.imageViewBottomSpacing)
+            $0.size.equalTo(Const.imageViewHeight)
+        }
+        
+        linkTitleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(15)
+            $0.leading.equalToSuperview().inset(17)
+            $0.bottom.equalToSuperview().inset(13)
+        }
+    }
+}
+
+private extension RecommendCell {
+    enum Const {
+        static let imageViewTopSpacing: CGFloat = 20
+        static let imageViewBottomSpacing: CGFloat = 20
+        static let imageViewHeight: CGFloat = 50
+        static let linkTitleLabelTopSpacing: CGFloat = 17
+        static let linkTitleLabelBottomSpacing: CGFloat = 13
+        static let linkTitleLabelHeight: CGFloat = 17
+        
+        static let discountAttributes = Typography.Attributes(
+            style: .body3,
+            weight: .medium,
+            textColor: .winey_purple400
+        )
+        static let titleAttributes = Typography.Attributes(
+            style: .body,
+            weight: .bold,
+            textColor: .winey_gray900
+        )
+        static let linkTitleAttributes = Typography.Attributes(
+            style: .body3,
+            weight: .medium,
+            textColor: .winey_gray600
+        )
+    }
+}
+
+extension RecommendCell {
+    static func cellHeight() -> CGFloat {
+        return Const.imageViewTopSpacing
+        + Const.imageViewHeight
+        + Const.imageViewBottomSpacing
+        + Const.linkTitleLabelTopSpacing
+        + Const.linkTitleLabelBottomSpacing
+        + Const.linkTitleLabelHeight
     }
 }
