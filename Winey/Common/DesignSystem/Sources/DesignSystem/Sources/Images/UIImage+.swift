b9cf8ecf5b9c0ac765d48817a36ed562a9b3e374
6uohul <kiy010604@naver.com>
2023-07-13 14:48:30 +0900

    [Fix] #3- 충돌 해결 및 디자인 시스템 적용

--- Winey/Common/DesignSystem/Sources/DesignSystem/Sources/Images/UIImage+.swift
+++ Winey/Common/DesignSystem/Sources/DesignSystem/Sources/Images/UIImage+.swift
@@ -13,5 +13,12 @@ public extension UIImage {
         public static let feed         = UIImage(named: "ic_feed")
         public static let recommend    = UIImage(named: "ic_recommend")
         public static let mypage       = UIImage(named: "ic_mypage")
+        public static let more = UIImage(named: "ic_more")
+        public static let like_unselected = UIImage(named: "ic_like_unselected")
+        public static let like_selected = UIImage(named: "ic_like_selected")
+    }
+    
+    enum Sample {
+        public static let temp = UIImage(named: "sample1")
     }
 }
