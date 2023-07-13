6fc6fe2698e64acca260837f2ba267962d744582
6uohul <kiy010604@naver.com>
2023-07-13 21:17:43 +0900

    [Fix] #3- 코드리뷰 반영

--- Winey/Common/DesignSystem/Sources/DesignSystem/Sources/Images/UIImage+.swift
+++ Winey/Common/DesignSystem/Sources/DesignSystem/Sources/Images/UIImage+.swift
@@ -10,13 +10,13 @@ import UIKit
 public extension UIImage {
 
     enum Icon {
-        public static let feed         = UIImage(named: "ic_feed")
-        public static let recommend    = UIImage(named: "ic_recommend")
-        public static let mypage       = UIImage(named: "ic_mypage")
-        public static let more = UIImage(named: "ic_more")
-        public static let like_unselected = UIImage(named: "ic_like_unselected")
-        public static let like_selected = UIImage(named: "ic_like_selected")
-        public static let floating = UIImage(named: "btn_floating")
+        public static let feed              = UIImage(named: "ic_feed")
+        public static let recommend         = UIImage(named: "ic_recommend")
+        public static let mypage            = UIImage(named: "ic_mypage")
+        public static let more              = UIImage(named: "ic_more")
+        public static let like_unselected   = UIImage(named: "ic_like_unselected")
+        public static let like_selected     = UIImage(named: "ic_like_selected")
+        public static let floating          = UIImage(named: "btn_floating")
     }
     
     enum Sample {
--- Winey/Winey/Source/Scenes/Feed/View/FeedHeaderView.swift
+++ Winey/Winey/Source/Scenes/Feed/View/FeedHeaderView.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+import DesignSystem
 import SnapKit
 
 final class FeedHeaderView: UICollectionReusableView {
