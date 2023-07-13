a288e2416651cdab39cd45bd3c0731ab0a5dd5c5
GodSamble <osgneb8359@naver.com>
2023-07-13 23:07:01 +0900

    [Fix] #4 - Asset 변수명 수정 및 적용

--- Winey/Winey/Source/Scenes/MyPage/Cells/Views/ProfileCollectionViewCell.swift
+++ Winey/Winey/Source/Scenes/MyPage/Cells/Views/ProfileCollectionViewCell.swift
@@ -31,6 +31,13 @@ final class ProfileCollectionViewCell: UICollectionViewCell {
         return containerView
     }()
     
+    var characterBackground: UIView = {
+        let containerView = UIView()
+        containerView.backgroundColor = UIColor.winey_purple100
+        containerView.layer.cornerRadius = 10
+        return containerView
+    }()
+    
     let levelLabel: UILabel = {
         let label = UILabel()
         label.text = "LV.기사"
@@ -43,7 +50,7 @@ final class ProfileCollectionViewCell: UICollectionViewCell {
     let infoButton: UIButton = {
         let button = UIButton()
         // TODO: 이미지 삽입
-        button.setImage(UIImage(named: "Group 1171275812"), for: .normal)
+        button.setImage(UIImage(named: "ic_info"), for: .normal)
         button.layer.shadowColor = UIColor.black.cgColor
         button.layer.shadowOffset = CGSize(width: 2, height: 2)
         button.layer.shadowOpacity = 0.5
@@ -57,14 +64,6 @@ final class ProfileCollectionViewCell: UICollectionViewCell {
         label.textColor = UIColor.winey_gray900
         label.font = .head_b28
         return label
-    }()
-    
-    var characterImage: UIImageView = {
-        let image = UIImageView()
-        // TODO: 이미지 삽입
-        image.image = UIImage(named: "Rectangle 6667428")
-        image.sizeToFit()
-        return image
     }()
     
     var subtitleContainerView: UIView = {
@@ -87,7 +86,7 @@ final class ProfileCollectionViewCell: UICollectionViewCell {
     private var progressbarImage: UIImageView = {
         let image = UIImageView()
         // TODO: 이미지 삽입
-        image.image = UIImage(named: "level_progressbar_i")
+        image.image = UIImage(named: "level_progressbar")
         image.sizeToFit()
         return image
     }()
@@ -96,51 +95,55 @@ final class ProfileCollectionViewCell: UICollectionViewCell {
     
     func setLayout() {
         
-        contentView.addSubviews(levelContainerView, infoButton, nicknameLabel, characterImage, subtitleContainerView, progressbarImage)
+        contentView.addSubviews(levelContainerView, infoButton, nicknameLabel, subtitleContainerView, progressbarImage, characterBackground)
         contentView.backgroundColor = .white
         levelContainerView.addSubview(levelLabel)
         subtitleContainerView.addSubview(subtitleLabel)
+        characterBackground.addSubview(subtitleContainerView)
         
-        subtitleLabel.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.bottom.equalTo(characterImage.snp.bottom).inset(13)
-        }
-        subtitleContainerView.snp.makeConstraints { make in
-            make.horizontalEdges.equalTo(subtitleLabel).inset(-8)
-            make.verticalEdges.equalTo(subtitleLabel).inset(-2)
-        }
-        subtitleContainerView.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.bottom.equalToSuperview().inset(65)
-            make.top.equalToSuperview().inset(250)
-        }
-        levelContainerView.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(12)
-            make.leading.equalToSuperview().offset(23)
+        characterBackground.snp.makeConstraints { make in
+            make.width.equalTo(358)
+            make.height.equalTo(196)
+            make.center.equalToSuperview().inset(16)
+            make.bottom.equalToSuperview().inset(52)
+            make.top.equalToSuperview().inset(91)
             
-            levelLabel.snp.makeConstraints { make in
-                make.verticalEdges.equalToSuperview().inset(4)
-                make.horizontalEdges.equalToSuperview().inset(10)
+            subtitleLabel.snp.makeConstraints { make in
+                make.centerX.equalToSuperview()
+                make.bottom.equalTo(characterBackground.snp.bottom).inset(13)
             }
-            
-            infoButton.snp.makeConstraints { make in
-                make.leading.equalTo(levelContainerView.snp.trailing)
+            subtitleContainerView.snp.makeConstraints { make in
+                make.horizontalEdges.equalTo(subtitleLabel).inset(-8)
+                make.verticalEdges.equalTo(subtitleLabel).inset(-2)
+            }
+            subtitleContainerView.snp.makeConstraints { make in
+                make.centerX.equalToSuperview()
+                make.bottom.equalToSuperview().inset(65)
+                make.top.equalToSuperview().inset(250)
+            }
+            levelContainerView.snp.makeConstraints { make in
                 make.top.equalToSuperview().offset(12)
-            }
-            
-            nicknameLabel.snp.makeConstraints { make in
                 make.leading.equalToSuperview().offset(23)
-                make.top.equalTo(levelLabel.snp.bottom).offset(8)
-            }
-            
-            characterImage.snp.makeConstraints { make in
-                make.top.equalTo(nicknameLabel.snp.bottom).offset(16)
-                make.leading.equalToSuperview().offset(16)
-            }
-            
-            progressbarImage.snp.makeConstraints { make in
-                make.top.equalTo(characterImage.snp.bottom).offset(13)
-                make.leading.equalToSuperview().offset(26)
+                
+                levelLabel.snp.makeConstraints { make in
+                    make.verticalEdges.equalToSuperview().inset(4)
+                    make.horizontalEdges.equalToSuperview().inset(10)
+                }
+                
+                infoButton.snp.makeConstraints { make in
+                    make.leading.equalTo(levelContainerView.snp.trailing)
+                    make.top.equalToSuperview().offset(12)
+                }
+                
+                nicknameLabel.snp.makeConstraints { make in
+                    make.leading.equalToSuperview().offset(23)
+                    make.top.equalTo(levelLabel.snp.bottom).offset(8)
+                }
+                
+                progressbarImage.snp.makeConstraints { make in
+                    make.top.equalTo(characterBackground.snp.bottom).offset(13)
+                    make.leading.equalToSuperview().offset(26)
+                }
             }
         }
     }
