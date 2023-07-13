00c8a9c053187a0da2b091026eaaa4483bbfe29f
6uohul <kiy010604@naver.com>
2023-07-14 03:02:40 +0900

    [Fix] #18- 추천 셀 수정

--- Winey/Winey/Source/Scenes/Recommend/View/RecommendCell.swift
+++ Winey/Winey/Source/Scenes/Recommend/View/RecommendCell.swift
@@ -7,6 +7,32 @@
 
 import UIKit
 
+import DesignSystem
+import SnapKit
+
 class RecommendCell: UICollectionViewCell {
     
+    private let testView = UIView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.backgroundColor = .winey_gray300
+        testView.backgroundColor = .winey_purple300
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+extension RecommendCell {
+    private func setLayout() {
+        contentView.addSubviews(testView)
+        
+        testView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
 }
