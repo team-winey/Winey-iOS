a9dbab07677a4690875f7375d39869c560d0d0fd
GodSamble <osgneb8359@naver.com>
2023-07-13 23:16:31 +0900

    [Chore] #4 - 마이페이지 구현 중, 필요 파일 생성과 불필요한 파일 제거

--- Winey/Winey/Source/Scenes/MyPage/Cells/CollectionViewFooter/LevelCollectionViewFooter.swift
+++
@@ -1,8 +0,0 @@
-//
-//  MypageCollectionViewFooter.swift
-//  Winey
-//
-//  Created by 고영민 on 2023/07/10.
-//
-
-import Foundation
--- Winey/Winey/Source/Scenes/MyPage/Cells/CollectionViewHeader/ProfileCollectionViewHeader.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ProfileHeader.swift
-//  Winey
-//
-//  Created by 고영민 on 2023/07/10.
-//
-
-import Foundation
