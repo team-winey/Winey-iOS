71b43daa6d97732f40ec6fd261be6403798b4e1f
6uohul <kiy010604@naver.com>
2023-07-15 00:45:30 +0900

    [Feat] #18- 추천 헤더뷰 구현

--- Winey/Winey/Source/Scenes/Recommend/View/RecommendHeaderView.swift
+++ Winey/Winey/Source/Scenes/Recommend/View/RecommendHeaderView.swift
@@ -10,13 +10,71 @@ import UIKit
 import DesignSystem
 import SnapKit
 
-class RecommendHeaderView: UICollectionReusableView {
+final class RecommendHeaderView: UICollectionReusableView {
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.setText(
+            "위니가 알려주는 추천 절약법",
+            attributes: .init(
+                style: .headLine3,
+                weight: .bold,
+                textColor: .winey_gray900
+            )
+        )
+        return label
+    }()
+    
+    private let containerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .winey_purple100
+        view.makeCornerRound(radius: 5)
+        return view
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.setText(
+            "쉿, 그대를 위한 절약방법이 도착했어.\n황실 내에서도 비밀인데 특별히 알려줄게:)",
+            attributes: .init(
+                style: .body3,
+                weight: .medium,
+                textColor: .winey_gray900
+            )
+        )
+        return label
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setLayout()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    private func setLayout(){
+        
+        self.addSubviews(titleLabel, containerView)
+        containerView.addSubview(descriptionLabel)
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(18)
+            $0.leading.equalToSuperview().inset(16)
+        }
+        
+        containerView.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(20)
+            $0.leading.trailing.equalToSuperview().inset(16)
+            $0.height.equalTo(77)
+        }
+        
+        descriptionLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().inset(17)
+        }
+    }
 }
+
