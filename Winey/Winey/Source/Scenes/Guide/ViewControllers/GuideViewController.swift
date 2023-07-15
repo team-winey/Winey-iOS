02070bea654133822008187ac3100c75f635073c
GodSamble <osgneb8359@naver.com>
2023-07-16 01:40:39 +0900

    [Feat] #24 - 사용법 (스크롤뷰) View Controller 구현

--- Winey/Winey/Source/Scenes/Guide/ViewControllers/GuideViewController.swift
+++ Winey/Winey/Source/Scenes/Guide/ViewControllers/GuideViewController.swift
@@ -5,4 +5,69 @@
 //  Created by 고영민 on 2023/07/15.
 //
 
-import Foundation
+import UIKit
+import DesignSystem
+import SnapKit
+
+final class GuideViewController: UIViewController {
+    
+    // MARK: - View Life Cycles
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+        setLayout()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    // MARK: - UI Components
+    
+    private let scrollView = UIScrollView()
+    private let bubbleView = BubbleView()
+    private let levelupMethodView = LevelupMethodView()
+    private let levelupRuleView = LevelupRuleView()
+    private let cautionView = CautionView()
+    
+    // MARK: - UI & Layout
+    
+    private func setUI() {
+        scrollView.backgroundColor = .winey_gray0
+    }
+    
+    private func setLayout() {
+        navigationController?.isNavigationBarHidden = true
+        view.addSubview(scrollView)
+        scrollView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        scrollView.addSubviews(bubbleView, levelupMethodView, levelupRuleView, cautionView)
+        
+        bubbleView.snp.makeConstraints { make in
+            make.height.equalTo(120)
+            make.top.leading.trailing.equalToSuperview()
+        }
+        
+        levelupMethodView.snp.makeConstraints { make in
+            make.height.equalTo(374)
+            make.top.equalTo(bubbleView.snp.bottom)
+            make.leading.trailing.equalToSuperview()
+        }
+        
+        levelupRuleView.snp.makeConstraints { make in
+            make.height.equalTo(554)
+            make.top.equalTo(levelupMethodView.snp.bottom)
+            make.leading.trailing.equalToSuperview()
+        }
+        
+        cautionView.snp.makeConstraints { make in
+            make.height.equalTo(206)
+            make.top.equalTo(levelupRuleView.snp.bottom)
+            make.leading.trailing.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
+    }
+}
