fce7ab6878145e555fc253893bf25d390af8577a
6uohul <kiy010604@naver.com>
2023-07-17 18:31:10 +0900

    [Feat] #30- NetworkConstant 추가

---
+++ Winey/Winey/Source/Network/Foundation/NetworkConstant.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkConstant.swift
+//  Winey
+//
+//  Created by 김인영 on 2023/07/17.
+//
+
+import Foundation
+
+struct NetworkConstant {
+    // userId는 5까지 아무거나 사용 가능
+    static let defaultHeader = ["Content-Type": "application/json", "userId": "2"]
+}
