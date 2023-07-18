a6333eac62f571bc74bd1c1632ae7925adf210f3
김응관 <kwhn9486@naver.com>
2023-07-19 06:16:46 +0900

    [Add] #38 MyFeedModel 파일 추가

--- Winey/Winey/Source/Scenes/Upload/ViewController/UploadViewController.swift
+++ Winey/Winey/Source/Scenes/Upload/ViewController/UploadViewController.swift
@@ -63,7 +63,7 @@ class UploadViewController: UIViewController {
     private let imagePicker = UIImagePickerController()
     
     /// feed가 업로드될 때 필요한 데이터들
-    private var feedImage: UIImage = UIImage()
+    private var feedImage: UIImage?
     private var feedTitle: String = "" {
         didSet {
             setButtonActivate(1)
@@ -359,7 +359,7 @@ class UploadViewController: UIViewController {
     /// 피드 업로드 함수
     @objc
     private func postData() {
-        print(feedImage)
+        print(feedImage!)
         print(feedTitle)
         print(feedPrice)
     }
