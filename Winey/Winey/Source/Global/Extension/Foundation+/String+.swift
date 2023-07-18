b41ce8128538bbc2b4b25f15be285fe58ac613d9
김응관 <kwhn9486@naver.com>
2023-07-18 17:40:28 +0900

    [Fix] #8 - 중복된 코드 삭제

--- Winey/Winey/Source/Global/Extension/Foundation+/String+.swift
+++ Winey/Winey/Source/Global/Extension/Foundation+/String+.swift
@@ -29,7 +29,7 @@ extension String {
         
         // 소수점이 있는 경우 처리
         if let _ = self.range(of: ".") {
-            var numberArray = self.components(separatedBy: ".")
+            let numberArray = self.components(separatedBy: ".")
             if numberArray.count == 1 {
                 var numberString = numberArray[0]
                 if numberString.isEmpty {
