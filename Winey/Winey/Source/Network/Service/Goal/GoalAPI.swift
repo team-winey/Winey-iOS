4c1a930aa450df35ee5b1f789bc1633225eb646e
6uohul <kiy010604@naver.com>
2023-07-20 08:13:07 +0900

    [Feat] #54- GoalAPI 추가

---
+++ Winey/Winey/Source/Network/Service/Goal/GoalAPI.swift
@@ -0,0 +1,45 @@
+//
+//  GoalAPI.swift
+//  Winey
+//
+//  Created by 김인영 on 2023/07/20.
+//
+
+import Foundation
+
+import Moya
+
+enum GoalAPI {
+    case postGoal(request: PostGoalRequest)
+}
+
+extension GoalAPI: TargetType {
+    var baseURL: URL {
+        return URL(string: URLConstant.baseURL)!
+    }
+    
+    var path: String {
+        switch self {
+        case .postGoal:
+            return URLConstant.goal
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .postGoal:
+            return .post
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .postGoal(let request):
+            return .requestJSONEncodable(request)
+        }
+    }
+    
+    var headers: [String : String]? {
+        return NetworkConstant.defaultHeader
+    }
+}
