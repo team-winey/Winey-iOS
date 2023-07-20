4d98e30eb089b4ea9b40fdb2ab29a2f766ba2c41
6uohul <kiy010604@naver.com>
2023-07-20 11:03:25 +0900

    [Feat] #56- 추천 API 추가

---
+++ Winey/Winey/Source/Network/Service/Recommend/RecommendAPI.swift
@@ -0,0 +1,48 @@
+//
+//  RecommendAPI.swift
+//  Winey
+//
+//  Created by 김인영 on 2023/07/20.
+//
+
+import Foundation
+
+import Moya
+
+enum RecommendAPI {
+    case getTotalRecommend(page: Int)
+}
+
+extension RecommendAPI: TargetType {
+    var baseURL: URL {
+        return URL(string: URLConstant.baseURL)!
+    }
+    
+    var path: String {
+        switch self {
+        case .getTotalRecommend:
+            return URLConstant.recommend
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .getTotalRecommend:
+            return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .getTotalRecommend(let page):
+            return .requestParameters(parameters: ["page": page], encoding: URLEncoding.queryString)
+        }
+    }
+    
+    var headers: [String : String]? {
+        switch self {
+        case .getTotalRecommend:
+            return NetworkConstant.defaultHeader
+        }
+    }
+}
