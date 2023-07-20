0487bb1594b38bff21cb079b7fe58136e8ddedc8
6uohul <kiy010604@naver.com>
2023-07-20 11:03:51 +0900

    [Feat] #56- 추천 Service 추가

---
+++ Winey/Winey/Source/Network/Service/Recommend/RecommendService.swift
@@ -0,0 +1,37 @@
+//
+//  RecommendService.swift
+//  Winey
+//
+//  Created by 김인영 on 2023/07/20.
+//
+
+import Foundation
+
+import Moya
+
+final class RecommendService {
+    
+    let recommendProvider = CustomMoyaProvider<RecommendAPI>()
+    
+    init() { }
+    
+    private(set) var recommendData: BaseResponse<TotalRecommendResponse>?
+    
+    // 1, 추천 절약법 조회
+    
+    func getTotalRecommend(page: Int, completion: @escaping (BaseResponse<TotalRecommendResponse>?) -> Void) {
+        recommendProvider.request(.getTotalRecommend(page: page)) { [self] (result) in
+            switch result {
+            case .success(let response):
+                do {
+                    self.recommendData = try response.map(BaseResponse<TotalRecommendResponse>.self)
+                    completion(recommendData)
+                } catch let error {
+                    print(error.localizedDescription, 500)
+                }
+            case .failure(let err):
+                print(err)
+            }
+        }
+    }
+}
