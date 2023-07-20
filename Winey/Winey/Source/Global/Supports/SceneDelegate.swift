f349972ccaf90aa8b6ea73a9938f6d416708f824
samsung-ga <maxwodyd98@gmail.com>
2023-07-20 22:37:05 +0900

    [Feat] #62 - User 정보 저장

--- Winey/Winey/Source/Global/Supports/SceneDelegate.swift
+++ Winey/Winey/Source/Global/Supports/SceneDelegate.swift
@@ -12,6 +12,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        UserSingleton.saveId(1)
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         self.window = window
