8cda9235cd424d113a673f2bb0bf7eba2893f30f
GodSamble <osgneb8359@naver.com>
2023-08-01 19:27:43 +0900

    [Chore] #82 - 카카오소셜로그인SPM설치 및 그 외 .xcode, .plist 파일 작업완료

--- Winey/Winey/Source/Global/Supports/AppDelegate.swift
+++ Winey/Winey/Source/Global/Supports/AppDelegate.swift
@@ -7,12 +7,13 @@
 
 import UIKit
 import DesignSystem
+import KakaoSDKCommon
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-       
+        KakaoSDK.initSDK(appKey: "1707e31a4de357b5f01aa386d17a7f12")
         Typography.Font.register()
         return true
     }
