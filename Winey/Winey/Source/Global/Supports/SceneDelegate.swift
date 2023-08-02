3a02d55d8a6e76dc9fb81afe5d8c0a8180e31968
김응관 <kwhn9486@naver.com>
2023-08-03 04:13:23 +0900

    [Feat] #85 - LoginView 제작 완료

--- Winey/Winey/Source/Global/Supports/SceneDelegate.swift
+++ Winey/Winey/Source/Global/Supports/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         self.window = window
-        let splashViewController = SplashViewController()
+        let splashViewController = LoginViewController()
         window.rootViewController = splashViewController
         window.makeKeyAndVisible()
     }
--- Winey/Winey/Source/Scenes/Login/ViewController/LoginViewController.swift
+++ Winey/Winey/Source/Scenes/Login/ViewController/LoginViewController.swift
@@ -12,6 +12,10 @@ import DesignSystem
 import SnapKit
 
 class LoginViewController: UIViewController {
+    
+    // MARK: - Properties
+    private let loginView = LoginView()
+    private lazy var safeArea = self.view.safeAreaLayoutGuide
 
     // MARK: - UI Components
     
@@ -28,6 +32,7 @@ class LoginViewController: UIViewController {
     // MARK: - Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUI()
         setLayout()
     }
     
@@ -36,11 +41,17 @@ class LoginViewController: UIViewController {
     }
     
     private func setLayout() {
-        view.addSubviews(kakaoButton, appleButton)
+        view.addSubviews(loginView, kakaoButton, appleButton)
+        
+        loginView.snp.makeConstraints {
+            $0.top.equalTo(safeArea).offset(72)
+            $0.leading.equalToSuperview().inset(40)
+            $0.trailing.equalToSuperview().inset(70)
+        }
         
         kakaoButton.snp.makeConstraints {
+            $0.top.equalTo(loginView.snp.bottom).offset(48)
             $0.horizontalEdges.equalToSuperview().inset(16)
-            $0.top.equalToSuperview().inset(500)
             $0.height.equalTo(54)
         }
         
