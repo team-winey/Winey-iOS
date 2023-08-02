259f8a873ae54bb40a1390e0061298d7d8e3c0f8
김응관 <kwhn9486@naver.com>
2023-08-02 16:14:36 +0900

    [Add] #85 - AuthenticationServices 라이브러리 추가

--- Winey/Winey/Source/Scenes/Login/ViewController/LoginViewController.swift
+++ Winey/Winey/Source/Scenes/Login/ViewController/LoginViewController.swift
@@ -5,25 +5,12 @@
 //  Created by 김응관 on 2023/08/02.
 //
 
+import AuthenticationServices
 import UIKit
 
 class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
