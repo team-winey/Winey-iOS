ab8f83761bc3a0b98fb6d0ca2bc4c7c6aec31d5c
김응관 <kwhn9486@naver.com>
2023-08-04 04:39:56 +0900

    [Add] #85 - AppleID 인증 모달 띄우고 정보를 가져오는 코드 추가

--- Winey/Winey/Source/Scenes/Login/ViewController/LoginViewController.swift
+++ Winey/Winey/Source/Scenes/Login/ViewController/LoginViewController.swift
@@ -12,7 +12,7 @@ import DesignSystem
 import SnapKit
 
 class LoginViewController: UIViewController {
-    
+
     // MARK: - Properties
     private let loginView = LoginView()
     private lazy var safeArea = self.view.safeAreaLayoutGuide
@@ -34,6 +34,11 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
         setUI()
         setLayout()
+        setAddTarget()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
     }
     
     private func setUI() {
@@ -59,6 +64,70 @@ class LoginViewController: UIViewController {
             $0.top.equalTo(kakaoButton.snp.bottom).offset(10)
             $0.horizontalEdges.equalTo(kakaoButton.snp.horizontalEdges)
             $0.height.equalTo(54)
+        }
+    }
+    
+    private func setAddTarget() {
+        appleButton.addTarget(self, action: #selector(appleLogin), for: .allTouchEvents)
+    }
+}
+
+extension LoginViewController: ASAuthorizationControllerDelegate,
+                               ASAuthorizationControllerPresentationContextProviding {
+    
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
+    }
+    
+    @objc
+    private func appleLogin() {
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        
+        switch authorization.credential {
+        case let appleIDCredential as ASAuthorizationAppleIDCredential:
+            
+            // Create an account in your system.
+            let userIdentifier = appleIDCredential.user
+            let fullName = appleIDCredential.fullName
+            let email = appleIDCredential.email
+            
+            if  let authorizationCode = appleIDCredential.authorizationCode,
+                let identityToken = appleIDCredential.identityToken,
+                let authCodeString = String(data: authorizationCode, encoding: .utf8),
+                let identifyTokenString = String(data: identityToken, encoding: .utf8) {
+                print("authCodeString: \(authCodeString)")
+                print("identifyTokenString: \(identifyTokenString)")
+            }
+            
+            print("useridentifier: \(userIdentifier)")
+            print("fullName: \(String(describing: fullName))")
+            print("email: \(String(describing: email))")
+            
+            //let validVC = SignValidViewController()
+            //validVC.modalPresentationStyle = .fullScreen
+            //present(validVC, animated: true, completion: nil)
+            
+        case let passwordCredential as ASPasswordCredential:
+            // 키체인 사용하여 로그인 -> 목업
+            let username = passwordCredential.user
+            let password = passwordCredential.password
+            
+            print("username: \(username)")
+            print("password: \(password)")
+            
+        default:
+            break
         }
     }
 }
