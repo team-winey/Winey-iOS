edb2edfb7a44d3533eccb9de24435f9b64f0ad2f
김응관 <kwhn9486@naver.com>
2023-08-04 04:51:56 +0900

    [Add] #85 - 로그인/회원가입을 위한 네트워크 기본사항 세팅 및 ResponseDTO 추가

---
+++ Winey/Winey/Source/Network/DTO/Login/LoginResponse.swift
@@ -0,0 +1,20 @@
+//
+//  LoginResponse.swift
+//  Winey
+//
+//  Created by 김응관 on 2023/08/04.
+//
+
+import Foundation
+
+struct LoginResponse {
+    let code: Int
+    let message: String
+    let data: UserData
+}
+
+struct UserData {
+    let userID: Int
+    let refreshToken, accessToken: String
+    let isRegistered: Bool
+}
