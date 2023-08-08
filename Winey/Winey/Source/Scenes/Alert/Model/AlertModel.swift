c53e92211e279162e10da46cbda59fc195209333
GodSamble <osgneb8359@naver.com>
2023-08-08 23:25:14 +0900

    [Feat] #86 - Alert 모델 임의구현

--- Winey/Winey/Source/Scenes/Alert/Model/AlertModel.swift
+++ Winey/Winey/Source/Scenes/Alert/Model/AlertModel.swift
@@ -5,4 +5,44 @@
 //  Created by 고영민 on 2023/08/08.
 //
 
-import Foundation
+import UIKit
+
+struct Alert {
+    let image: UIImage
+    let kind: String
+    let content: Content // TODO: 이미자하고 텍스트가 바뀜
+    let elapsedTime: String
+}
+
+extension Alert {
+    static func dummy() -> [Alert] {
+        return [Alert(image: .Icon.like!,
+                      kind: "좋아요",
+                      content: .levelup,
+                      elapsedTime: "10분전"),
+                Alert(image: .Icon.like!,
+                      kind: "좋아요",
+                      content: .levelup,
+                      elapsedTime: "10분전"),
+                Alert(image: .Icon.like!,
+                      kind: "좋아요",
+                      content: .levelup,
+                      elapsedTime: "10분전"),
+                Alert(image: .Icon.like!,
+                      kind: "좋아요",
+                      content: .levelup,
+                      elapsedTime: "10분전"),
+                Alert(image: .Icon.like!,
+                      kind: "좋아요",
+                      content: .levelup,
+                      elapsedTime: "10분전"),
+                Alert(image: .Icon.like!,
+                      kind: "좋아요",
+                      content: .levelup,
+                      elapsedTime: "10분전"),
+                Alert(image: .Icon.like!,
+                      kind: "좋아요",
+                      content: .levelup,
+                      elapsedTime: "10분전")]
+    }
+}
