b36d694e2249f88b1d1ac644fb23c5ecfa2f0665
6uohul <kiy010604@naver.com>
2023-08-17 22:14:14 +0900

    [Fix] #90- Network 헤더 수정

--- Winey/Winey/Source/Network/Base/NetworkConstant.swift
+++ Winey/Winey/Source/Network/Base/NetworkConstant.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct NetworkConstant {
     static let defaultHeader = ["Content-Type": "application/json",
-                                "accessToken": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJ0b2tlbiIsImlhdCI6MTY5MjExMzQ0NCwiZXhwIjoxNzAwNzUzNDQ0LCJ1c2VySWQiOiIzMiJ9.PyjRnBGk2ZcIwas2QOb4KYFczQQ3KpCpNithiMkTmK2evtMqstAJWc1wF_brWzNk"]
-    static let postfeedHeader = ["Content-Type": "multipart/form-data",
-                                 "accessToken": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJ0b2tlbiIsImlhdCI6MTY5MjExMzQ0NCwiZXhwIjoxNzAwNzUzNDQ0LCJ1c2VySWQiOiIzMiJ9.PyjRnBGk2ZcIwas2QOb4KYFczQQ3KpCpNithiMkTmK2evtMqstAJWc1wF_brWzNk"]
+                                    "accessToken": KeychainManager.shared.read("accessToken")!]
+        static let postfeedHeader = ["Content-Type": "multipart/form-data",
+                                     "accessToken": KeychainManager.shared.read("accessToken")!]
 }
