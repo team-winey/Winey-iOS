05b9a26ead096b7d400c1d2fc0e0180f9aadbe48
6uohul <kiy010604@naver.com>
2023-08-17 22:08:03 +0900

    [Fix] #90- 실기기 연결

--- Winey/Winey/Source/Global/Supports/SceneDelegate.swift
+++ Winey/Winey/Source/Global/Supports/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         self.window = window
-        let splashViewController = FeedViewController()
+        let splashViewController = SplashViewController()
         window.rootViewController = splashViewController
         window.makeKeyAndVisible()
     }
