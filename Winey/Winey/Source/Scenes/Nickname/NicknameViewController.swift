46982a6a88dd371fb72186e12c31eccaa33f07a2
김응관 <kwhn9486@naver.com>
2023-08-18 15:39:02 +0900

    [Feat] #112 - 닉네임 설정 뷰 UI 제작 완료

--- Winey/Winey/Source/Scenes/Nickname/NicknameViewController.swift
+++ Winey/Winey/Source/Scenes/Nickname/NicknameViewController.swift
@@ -7,23 +7,167 @@
 
 import UIKit
 
+import DesignSystem
+import SnapKit
+
 class NicknameViewController: UIViewController {
+    
+    private let duplicateCheckBtn = DuplicateCheckButton()
+    private let nickNameTextField = WITextFieldView(type: .nickName)
+    private let navigationBar = WINavigationBar(leftBarItem: .close)
+    
+    // MARK: - UI Components
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.textAlignment = .left
+        return label
+    }()
+    
+    private let subTitle: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.textAlignment = .left
+        return label
+    }()
+    
+    private let nextButton: MIButton = {
+        let btn = MIButton(type: .yellow)
+        btn.isEnabled = false
+        btn.setTitle("확인", for: .normal)
+        return btn
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUI()
+        setLayout()
+        setGuideText()
+        setNotification()
+        setAddTarget()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setUI() {
+        view.backgroundColor = .winey_gray0
     }
-    */
+    
+    private func setLayout() {
+        view.addSubviews(navigationBar, titleLabel, subTitle, duplicateCheckBtn, nickNameTextField, nextButton)
+        
+        navigationBar.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.horizontalEdges.equalToSuperview()
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview().inset(24)
+            $0.top.equalTo(view.safeAreaLayoutGuide).inset(70)
+        }
+        
+        subTitle.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(7)
+            $0.horizontalEdges.equalToSuperview().inset(24)
+        }
+        
+        duplicateCheckBtn.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(24)
+            $0.leading.equalTo(nickNameTextField.snp.trailing).offset(8)
+            $0.top.equalTo(subTitle.snp.bottom).offset(42)
+            $0.height.equalTo(56)
+        }
+        
+        nickNameTextField.snp.makeConstraints {
+            $0.top.equalTo(subTitle.snp.bottom).offset(42)
+            $0.leading.equalToSuperview().inset(24)
+            $0.height.equalTo(56)
+            $0.trailing.equalToSuperview().inset(112)
+        }
+        
+        nextButton.snp.makeConstraints {
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(4)
+            $0.height.equalTo(52)
+            $0.horizontalEdges.equalToSuperview().inset(10)
+        }
+    }
+    
+    private func setGuideText() {
+        titleLabel.setText("위니제국에 들어온 당신,\n어떤 닉네임으로 불리고 싶나요?", attributes: Const.titleAttributes)
+        subTitle.setText("공백 또는 특수문자는 사용할 수 없습니다",
+                         attributes: Const.subTitleAttributes)
+    }
+    
+    private func setNotification() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillHide),
+            name: UIResponder.keyboardWillHideNotification,
+            object:nil)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    private func setAddTarget() {
+        navigationBar.leftButton.addTarget(self, action: #selector(tapLeftButton), for: .touchUpInside)
+    }
+    
+    @objc
+    private func tapLeftButton() {
+        self.dismiss(animated: true)
+    }
+    
+    @objc
+    private func keyboardWillShow(notification: NSNotification) {
+        
+        guard let userInfo = notification.userInfo, let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
+        
+        var bottomInset: CGFloat = 0
+        
+        if #available(iOS 11.0, *) {
+            bottomInset = view.safeAreaInsets.bottom
+        }
+        
+        let adjustedBottomSpace = keyboardFrame.size.height - bottomInset + 14
+        self.nextButton.snp.updateConstraints { make in
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
+                .inset(adjustedBottomSpace)
+        }
+        view.layoutIfNeeded()
+    }
+    
+    @objc
+    private func keyboardWillHide(notification: NSNotification) {
+        self.nextButton.snp.updateConstraints { make in
+            make.bottom.equalTo(view.safeAreaLayoutGuide).inset(12)
+        }
+        view.layoutIfNeeded()
+    }
+}
 
+private extension NicknameViewController {
+    enum Const {
+        static let titleAttributes = Typography.Attributes(
+            style: .headLine2,
+            weight: .bold,
+            textColor: .winey_gray900
+        )
+        
+        static let subTitleAttributes = Typography.Attributes(
+            style: .body3,
+            weight: .medium,
+            textColor: .winey_gray400
+        )
+    }
+    
+    enum NicknameType {
+        
+    }
 }
