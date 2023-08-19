60a7c5e6cf7182777184c5e4d18530613c774ab5
6uohul <kiy010604@naver.com>
2023-08-19 14:19:42 +0900

    [Feat] #117- VC에 컬렉션 뷰 추가

--- Winey/Winey/Source/Global/Supports/SceneDelegate.swift
+++ Winey/Winey/Source/Global/Supports/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         self.window = window
-        let splashViewController = SplashViewController()
+        let splashViewController = AnimationOnboardingViewController()
         window.rootViewController = splashViewController
         window.makeKeyAndVisible()
     }
