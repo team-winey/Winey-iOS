b711f37737d1f37c56826f6fbf6476227ae2e1e5
김응관 <kwhn9486@naver.com>
2023-08-19 23:33:18 +0900

    [Add] #122 - 닉네임 설정 ResponseDTO 추가

---
+++ Winey/Winey/Source/Network/DTO/Nickname/NicknameResponse.swift
@@ -0,0 +1,14 @@
+//
+//  NicknameResponse.swift
+//  Winey
+//
+//  Created by 김응관 on 2023/08/19.
+//
+
+import Foundation
+
+struct NicknameResponse {
+    let code: Int
+    let message: String
+    let data: NSNull
+}
