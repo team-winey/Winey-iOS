3d14b80ba729a0ea137159a1f6d5913cd15fdf61
6uohul <kiy010604@naver.com>
2023-08-20 04:07:12 +0900

    [Feat] #118- 읽음 여부 확인 responseDTO 생성

---
+++ Winey/Winey/Source/Network/DTO/Notification/CheckNewNotificationResponse.swift
@@ -0,0 +1,12 @@
+//
+//  CheckNewNotificationResponse.swift
+//  Winey
+//
+//  Created by 김인영 on 2023/08/20.
+//
+
+import Foundation
+
+struct CheckNewNotificationResponse: Codable {
+    let hasNewNotification: Bool
+}
