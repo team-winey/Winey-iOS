25fa107f9342ddd1485ad7e7968ace0e5a016614
김응관 <kwhn9486@naver.com>
2023-08-26 19:14:37 +0900

    [Add] #159 - 앱이 background에서 foreground로 진입할때마다 토큰 재발급 로직 실행되는 코드추가

--- Winey/Winey/Source/Global/Supports/SceneDelegate.swift
+++ Winey/Winey/Source/Global/Supports/SceneDelegate.swift
@@ -8,6 +8,8 @@
 import UIKit
 import KakaoSDKAuth
 
+import DesignSystem
+
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
@@ -28,5 +30,42 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                 _ = AuthController.handleOpenUrl(url: url)
             }
         }
+    }
+    
+    // 앱을 background에서 foreground로 불러올 경우
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        guard let refreshToken = KeychainManager.shared.read("refreshToken") else { return }
+        print("background에서 foreground로 진입")
+        
+        print("리프레쉬 토큰을 통한 액세스 토큰 재발급")
+        LoginService.shared.reissueApple(token: refreshToken) { result in
+            switch result {
+            case true:
+                print("토큰 재발급 성공!")
+            case false:
+                print("refreshToken 만료")
+                print("토큰 재발급 실패 -> LoginView로 변환")
+                guard let windowScene = (scene as? UIWindowScene) else { return }
+                let window = UIWindow(windowScene: windowScene)
+                self.window = window
+                self.setAlert(window)
+            }
+        }
+    }
+    
+    func setAlert(_ window: UIWindow) {
+        let alert = MIPopupViewController(
+            content: .init(
+                title: "세션이 만료되었습니다",
+                subtitle: "확인 버튼을 눌러서 다시 로그인을 진행해주세요"
+            )
+        )
+        alert.addButton(title: "확인", type: .yellow) {
+            window.rootViewController = LoginViewController()
+            window.makeKeyAndVisible()
+        }
+        window.rootViewController = alert
+        window.makeKeyAndVisible()
+        window.backgroundColor = .winey_gray0
     }
 }
