981815caf233e1adaa3cb6f98b5894229902b035
김응관 <kwhn9486@naver.com>
2023-09-02 20:17:30 +0900

    [Fix] #166 - GalleryCell 레이아웃 및 디자인 수정

--- Winey/Winey/Source/Scenes/Gallery/View/Cell/GalleryCell.swift
+++ Winey/Winey/Source/Scenes/Gallery/View/Cell/GalleryCell.swift
@@ -12,7 +12,7 @@ import SnapKit
 
 class GalleryCell: UICollectionViewCell {
     
-    var label: UILabel = {
+    private let label: UILabel = {
         let label = UILabel()
         label.setText("접근 권한이 허용된 사진",
                       attributes: .init(style: .body3,
@@ -21,9 +21,37 @@ class GalleryCell: UICollectionViewCell {
         return label
     }()
     
+    private let imageCount: UILabel = {
+        let label = UILabel()
+        label.setText("0",
+                      attributes: .init(style: .body3, weight: .medium))
+        label.sizeToFit()
+        return label
+    }()
+    
+    private let stackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.spacing = 10
+        stack.alignment = .leading
+        return stack
+    }()
+    
+    private let thumbnailImg: UIImageView = {
+        let img = UIImageView()
+        img.image = UIImage(systemName: "photo")?.resizeWithWidth(width: 40)?.withTintColor(.winey_gray400)
+        return img
+    }()
+    
+    private let thumbnailContentView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .winey_gray200
+        return view
+    }()
+    
     private let arrow: UIImageView = {
         let img = UIImageView()
-        img.image = UIImage(systemName: "chevron.right")
+        img.image = UIImage.Mypage.next
         return img
     }()
     
@@ -38,15 +66,40 @@ class GalleryCell: UICollectionViewCell {
     }
     
     private func setUI() {
-        contentView.backgroundColor = .red
+        contentView.backgroundColor = .winey_gray0
     }
     
     private func setLayout() {
-        contentView.addSubview(arrow)
+        stackView.addArrangedSubviews(label, imageCount)
+        thumbnailContentView.addSubview(thumbnailImg)
+        contentView.addSubviews(thumbnailContentView, stackView, arrow)
+        
+        label.snp.makeConstraints {
+            $0.top.leading.equalToSuperview()
+        }
+        
+        imageCount.snp.makeConstraints {
+            $0.leading.bottom.equalToSuperview()
+        }
+        
+        thumbnailImg.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+        
+        thumbnailContentView.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(24)
+            $0.height.width.equalTo(60)
+            $0.centerY.equalToSuperview()
+        }
+        
+        stackView.snp.makeConstraints {
+            $0.leading.equalTo(thumbnailImg.snp.trailing).offset(20)
+            $0.centerY.equalToSuperview()
+        }
         
         arrow.snp.makeConstraints {
-            $0.centerX.equalToSuperview()
-            $0.width.height.equalTo(20)
+            $0.trailing.equalToSuperview().inset(24)
+            $0.centerY.equalToSuperview()
         }
     }
 }
