c53a1b8ad4415fb1733be8e41ed1d45aa5bead6f
김응관 <kwhn9486@naver.com>
2023-09-03 01:46:16 +0900

    [Feat] #166 - 갤러리 썸네일에 썸네일 이미지와 접근권한 허용된 이미지 개수 보여주는 기능 구현

--- Winey/Winey/Source/Scenes/Gallery/ViewController/GalleryViewController.swift
+++ Winey/Winey/Source/Scenes/Gallery/ViewController/GalleryViewController.swift
@@ -5,6 +5,7 @@
 //  Created by 김응관 on 2023/08/31.
 //
 
+import Combine
 import UIKit
 import Photos
 
@@ -20,7 +21,11 @@ class GalleryViewController: UIViewController {
                       height: (((UIScreen.main.bounds.width) - (imageSpace * 3)) / 4))
     }
     
+    private var tumbnailImg: UIImage = UIImage()
+    private var imgCount: Int = 0
+    
     private let navigationBar = WINavigationBar(leftBarItem: .back, title: "Photos")
+    private var bag = Set<AnyCancellable>()
     
     private let flowLayout: UICollectionViewLayout = {
         let flowLayout = UICollectionViewFlowLayout()
@@ -35,18 +40,32 @@ class GalleryViewController: UIViewController {
         view.backgroundColor = .winey_gray0
         return view
     }()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setPhotoFetch()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setPhotoFetch()
         setCollectionView()
         setUI()
         setAddTarget()
         setLayout()
+        bind()
     }
     
     deinit {
         PHPhotoLibrary.shared().unregisterChangeObserver(self)
+    }
+    
+    func bind() {
+        NotificationCenter.default.publisher(for: .whenEnterForeground)
+            .sink(receiveValue: { [weak self] _ in
+                self?.setPhotoFetch()
+                self?.collectionView.reloadData()
+            })
+            .store(in: &bag)
     }
     
     func setUI() {
@@ -78,9 +97,38 @@ class GalleryViewController: UIViewController {
     
     private func setPhotoFetch() {
         let allPhotosOptions = PHFetchOptions()
-        allPhotosOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
+        allPhotosOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
         allPhotos = PHAsset.fetchAssets(with: allPhotosOptions)
+        sendTunmbnail(allPhotos.firstObject ?? PHAsset())
+        setImageCount(allPhotos.count)
         PHPhotoLibrary.shared().register(self)
+    }
+    
+    private func sendTunmbnail(_ target: PHAsset) {
+        
+        let requestOptions = PHImageRequestOptions()
+        requestOptions.deliveryMode = .highQualityFormat
+        requestOptions.isNetworkAccessAllowed = true
+        requestOptions.isSynchronous = true
+        requestOptions.resizeMode = .exact
+        
+        PHCachingImageManager().startCachingImages(for: [target],
+                                                   targetSize: CGSize(width: 80, height: 80),
+                                                   contentMode: .aspectFill,
+                                                   options: requestOptions)
+        
+        PHImageManager().requestImage(for: target,
+                                  targetSize: CGSize(width: 80, height: 80),
+                                  contentMode: .aspectFill,
+                                  options: requestOptions, resultHandler: { [weak self] image, _ in
+            guard let image = image else { return }
+            guard let self = self else { return }
+            self.tumbnailImg = image
+        })
+    }
+    
+    private func setImageCount(_ count: Int) {
+        imgCount = count
     }
     
     @objc
@@ -92,7 +140,9 @@ class GalleryViewController: UIViewController {
 extension GalleryViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: GalleryCell.className, for: indexPath)
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: GalleryCell.className, for: indexPath) as? GalleryCell else { return GalleryCell() }
+        cell.configure(tumbnailImg, imgCount)
         return cell
     }
     
@@ -116,6 +166,8 @@ extension GalleryViewController: PHPhotoLibraryChangeObserver {
         DispatchQueue.main.sync {
             if let changeDetails = changeInstance.changeDetails(for: allPhotos) {
                 allPhotos = changeDetails.fetchResultAfterChanges
+                sendTunmbnail(allPhotos.firstObject ?? PHAsset())
+                setImageCount(allPhotos.count)
             }
         }
     }
