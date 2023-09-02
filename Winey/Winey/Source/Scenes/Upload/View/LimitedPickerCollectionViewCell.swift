ab3ed01bd04362daa2255594a140feec2e01fd1c
김응관 <kwhn9486@naver.com>
2023-09-02 17:32:36 +0900

    [Feat] #166 - 접근권한 허용된 사진들 하나하나를 보여주는 셀 정의

---
+++ Winey/Winey/Source/Scenes/Upload/View/LimitedPickerCollectionViewCell.swift
@@ -0,0 +1,61 @@
+//
+//  LimitedPickerCollectionViewCell.swift
+//  Winey
+//
+//  Created by 김응관 on 2023/08/31.
+//
+
+import UIKit
+
+import SnapKit
+
+class LimitedPickerCollectionViewCell: UICollectionViewCell {
+        
+    // MARK: - UI Properties
+    
+    var representedAssetIdentifier: String?
+    var thumbnailImage: UIImage = UIImage() {
+        didSet {
+            imageView.image = thumbnailImage
+        }
+    }
+    
+    var originalSize: CGSize = CGSize() 
+    
+    let imageView: UIImageView = {
+        let view = UIImageView()
+        view.clipsToBounds = true
+        view.contentMode = .scaleAspectFill
+        return view
+    }()
+
+    // MARK: - Life Cycle
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension LimitedPickerCollectionViewCell {
+    
+    // MARK: - Layout
+    
+    private func setupLayout() {
+        contentView.addSubview(imageView)
+        
+        imageView.snp.makeConstraints {
+            $0.edges.equalTo(self.safeAreaLayoutGuide)
+        }
+    }
+    
+    // MARK: - Method
+    
+    func getImage() -> UIImage {
+        return imageView.image ?? UIImage()
+    }
+}
